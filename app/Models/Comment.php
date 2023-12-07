b29c26d5084871a65c86ab56a47cce0db2d6140c
Ertinho <105566974+Ertinho@users.noreply.github.com>
2023-12-06 22:23:29 -0300

    feat(model/Comment): add attributes and relationships

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'text',
+        'user_id',
+        'post_id',
+    ];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+
+    }
+
 }
